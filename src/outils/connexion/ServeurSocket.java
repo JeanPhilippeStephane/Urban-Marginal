2f0f1d6ff12739bcd17aaba85d7cb2d962e62d5f
Stéphane <Stphane@DESKTOP-5U560PK>
2016-09-21 21:24:18 +0200

    classe ServerSocket terminé

--- src/outils/connexion/ServeurSocket.java
+++ src/outils/connexion/ServeurSocket.java
@@ -1,11 +1,34 @@
 package outils.connexion;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-public class ServeurSocket {
+public class ServeurSocket extends Thread {
 	private Object leRecepteur;
 	private ServerSocket serveurSocket;
 	public ServeurSocket(Object leRecepteur, int port){
-		
+		try {
+			serveurSocket=new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("erreur grave création socket serveur : "+e);
+			System.exit(0);
+		}
+		this.leRecepteur= leRecepteur;
+		start();
+	}
+	public void run(){
+		Socket socket;
+		while(true){
+			try {
+				socket=serveurSocket.accept();
+				 System.out.println("le serveur attend");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("erreur grave gestion socket :"+e);
+				System.exit(0);
+			}
+		}
 	}
 }
